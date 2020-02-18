da96e9106b3dcd21583ebebf4d0bca3501d9eacb
Paweł Prażak <pawelprazak@gmail.com>
2020-02-18 15:50:46 +0100

    create a test namespace

--- src/main/scala/com/virutslab/DSLMain.scala
+++ src/main/scala/com/virutslab/DSLMain.scala
@@ -5,6 +5,9 @@ import akka.stream.ActorMaterializer
 import skuber.api.Configuration
 import skuber.api.client.Context
 
+import scala.concurrent.ExecutionContextExecutor
+import scala.util.{Failure, Success}
+
 abstract class DSLMain {
 
   import skuber._
@@ -12,10 +15,22 @@ abstract class DSLMain {
 
   implicit private val system: ActorSystem = ActorSystem("")
   implicit private val actorMaterializer: ActorMaterializer = ActorMaterializer()
+  implicit private val dispatcher: ExecutionContextExecutor = system.dispatcher
 
   private val kubeconfig: Configuration = api.Configuration.parseKubeconfigFile().get
   private val ourContext: Context = kubeconfig.contexts("gke_infrastructure-as-types_us-central1-a_standard-cluster-1")
   private val configWithContext = kubeconfig.useContext(ourContext)
 
   private val client: K8SRequestContext = k8sInit(config = configWithContext, appConfig = system.settings.config)
+
+  private val namespace = Namespace.forName("test")
+
+  private val create = for {
+    ns <- client.create(namespace)
+  } yield ns
+
+  create.onComplete {
+    case Success(_)  => println("Successfully created resources on Kubernetes cluster")
+    case Failure(ex) => throw new Exception("Encountered exception trying to create resources on Kubernetes cluster: ", ex)
+  }
 }
