e35db1ae3c729551f1a88ebc1d4cd10aca8442c0
Bartosz Kowalik <bkowalik@virtuslab.com>
2020-03-16 12:23:18 +0100

    Refactor connection

--- src/main/scala/com/virtuslab/dsl/Connection.scala
+++ src/main/scala/com/virtuslab/dsl/Connection.scala
@@ -1,20 +1,13 @@
 package com.virtuslab.dsl
 
-abstract class Connection[A: Selectable, B: Selectable, C: Selectable] extends Namespaced {
-  def namespace: Namespace
-  def resourceSelector: Selector[A]
-  def ingress: Selector[B]
-  def egress: Selector[C]
-}
+case class Connection[A: Selectable, B: Selectable, C: Selectable](
+    namespace: Namespace,
+    resourceSelector: Selector[A],
+    ingress: Selector[B],
+    egress: Selector[C])
+  extends Namespaced
 
 object Connection {
-  private case class DefinedConnection[A: Selectable, B: Selectable, C: Selectable](
-      namespace: Namespace,
-      resourceSelector: Selector[A],
-      ingress: Selector[B],
-      egress: Selector[C])
-    extends Connection[A, B, C]
-
   def apply[A: Selectable, B: Selectable, C: Selectable](
       resourceSelector: Selector[A],
       ingress: Selector[B] = EmptySelector,
@@ -22,7 +15,7 @@ object Connection {
     )(implicit
       builder: NamespaceBuilder
     ): Connection[A, B, C] = {
-    val conn = DefinedConnection(builder.namespace, resourceSelector, ingress, egress)
+    val conn = Connection(builder.namespace, resourceSelector, ingress, egress)
     builder.connections(conn)
     conn
   }
