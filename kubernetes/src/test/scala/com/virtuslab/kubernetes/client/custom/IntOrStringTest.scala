cd2ab4563c29bec83b6b1aa87b54dc42ed1e47b5
Paweł Prażak <pawelprazak@gmail.com>
2020-04-10 20:25:06 +0200

    cleanup tests

--- kubernetes/src/test/scala/com/virtuslab/kubernetes/client/custom/IntOrStringTest.scala
+++ kubernetes/src/test/scala/com/virtuslab/kubernetes/client/custom/IntOrStringTest.scala
@@ -2,13 +2,12 @@ package com.virtuslab.kubernetes.client.custom
 
 import org.json4s.{ Formats, NoTypeHints }
 import org.json4s.jackson.Serialization
-import org.specs2.matcher.JsonMatchers
 import org.specs2.mutable.Specification
 
-class IntOrStringTest extends Specification with JsonMatchers {
-  "Unit specification for the IntOrString model. ".txt
+class IntOrStringTest extends Specification {
+  "Unit specification for the IntOrString model. ".br
 
-  "An IntOrString can be constructed form an Int\n" >> {
+  "An IntOrString can be constructed form an Int" >> {
     "where value is 0" >> { IntOrString(0).int mustEqual 0 }
     "where value is 1" >> { IntOrString(1).int mustEqual 1 }
     "where value is -1" >> { IntOrString(-1).int mustEqual -1 }
@@ -22,12 +21,12 @@ class IntOrStringTest extends Specification with JsonMatchers {
   import org.json4s.jackson.Serialization._
   implicit val formats: Formats = Serialization.formats(NoTypeHints) + IntOrString.Serializer
 
-  "An IntOrString can be marshalled to JSON\n" >> {
+  "An IntOrString can be marshalled to JSON" >> {
     "where a value of '10' is used" >> { write(IntOrString(10)) mustEqual "10" }
     "where a value of 'ten' is used" >> { write(IntOrString("ten")) mustEqual "\"ten\"" }
   }
 
-  "An IntOrString can be un-marshalled from JSON\n" >> {
+  "An IntOrString can be un-marshalled from JSON" >> {
     "where a value of '10' is used" >> { read[IntOrString]("10") mustEqual IntOrString(10) }
     "where a value of 'ten' is used" >> { read[IntOrString]("\"ten\"") mustEqual IntOrString("ten") }
   }
--- kubernetes/src/test/scala/com/virtuslab/kubernetes/client/custom/QuantityTest.scala
+++ kubernetes/src/test/scala/com/virtuslab/kubernetes/client/custom/QuantityTest.scala
@@ -1,16 +1,14 @@
 package com.virtuslab.kubernetes.client.custom
 
-import com.google.gson.Gson
 import org.json4s.jackson.Serialization
 import org.json4s.{ Formats, NoTypeHints }
 import org.specs2.mutable.Specification
-import org.specs2.matcher.JsonMatchers
 
 // Adapted from skuber
-class QuantityTest extends Specification with JsonMatchers {
-  "This is a unit specification for the Quantity model. ".txt
+class QuantityTest extends Specification {
+  "This is a unit specification for the Quantity model. ".br
 
-  "A quantity can be constructed from decimal SI values\n" >> {
+  "A quantity can be constructed from decimal SI values" >> {
     "where a value of 100m equates to 0.1" >> { Quantity("100m").amount mustEqual 0.1 }
     "where a value of 100k equates to 100000" >> { Quantity("100k").amount mustEqual 100000 }
     "where a value of 100M equates to 100000000" >> { Quantity("100M").amount mustEqual 100000000 }
@@ -20,7 +18,7 @@ class QuantityTest extends Specification with JsonMatchers {
     "where a value of 100E equates to 100E+18" >> { Quantity("100E").amount mustEqual 100e+18 }
   }
 
-  "A quantity can be constructed from values with scientific E notation\n" >> {
+  "A quantity can be constructed from values with scientific E notation" >> {
     "where a value of 0.01E+5 equates to 1000" >> { Quantity("0.01E+5").amount mustEqual 1000 }
     "where a value of 10010.56E-3 equates to 10.01056" >> { Quantity("10010.56E-3").amount mustEqual 10.01056 }
     "where a value of 55.67e+6 equates to 55670000" >> { Quantity("55.67e+6").amount mustEqual 55670000 }
@@ -28,7 +26,7 @@ class QuantityTest extends Specification with JsonMatchers {
     "where a value of 67700e-33 equates to 67.700" >> { Quantity("67700e-3").amount mustEqual 67.700 }
   }
 
-  "A quantity can be constructed from binary SI values\n" >> {
+  "A quantity can be constructed from binary SI values" >> {
     "where a value of 100Ki equates to 102400" >> { Quantity("100Ki").amount mustEqual 102400 }
     "where a value of 10Mi equates to 10485760" >> { Quantity("10Mi").amount mustEqual 10485760 }
     "where a value of 10Ti equates to 10 *(2 ^ 40) " >> { Quantity("10Ti").amount mustEqual 10 * Math.pow(2, 40) }
@@ -43,14 +41,14 @@ class QuantityTest extends Specification with JsonMatchers {
     }
   }
 
-  "A quantity can be constructed for plain integer and decimal values with no suffixes\n" >> {
+  "A quantity can be constructed for plain integer and decimal values with no suffixes" >> {
     "where a value of 10 is valid" >> { Quantity("10").amount mustEqual 10 }
     "where a value of -10 is valid" >> { Quantity("-10").amount mustEqual -10 }
     "where a value of 10.55 is valid" >> { Quantity("10.55").amount mustEqual 10.55 }
     "where a value of -10.55 is valid" >> { Quantity("-10.55").amount mustEqual -10.55 }
   }
 
-  "A quantity will reject bad values\n" >> {
+  "A quantity will reject bad values" >> {
     "where constructing from a value of 10Zi results in an exception" >> {
       def badVal: BigDecimal = Quantity("10Zi").amount
       badVal must throwAn[Exception]
@@ -64,12 +62,12 @@ class QuantityTest extends Specification with JsonMatchers {
   import org.json4s.jackson.Serialization._
   implicit val formats: Formats = Serialization.formats(NoTypeHints) + Quantity.Serializer
 
-  "A quantity can be marshalled to JSON\n" >> {
+  "A quantity can be marshalled to JSON" >> {
     "where a value of 100Ki is used" >> { write(Quantity("100Ki")) mustEqual ("\"100Ki\"") }
     "where a value of 10 is used" >> { write(Quantity("10")) mustEqual ("\"10\"") }
   }
 
-  "A quantity can be un-marshalled from JSON\n" >> {
+  "A quantity can be un-marshalled from JSON" >> {
     "where a value of 100Ki is used" >> { read[Quantity]("\"100Ki\"") mustEqual Quantity("100Ki") }
   }
 }
