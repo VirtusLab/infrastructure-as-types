f222e576dae470e71e69a61b92099a0c32ad2d7b
Paweł Prażak <pawelprazak@gmail.com>
2020-04-11 21:45:57 +0200

    Add OpenAPIContext, NamespaceInterpreter

---
+++ dsl/src/main/scala/com/virtuslab/interpreter/openapi/OpenAPI.scala
@@ -0,0 +1,30 @@
+package com.virtuslab.interpreter.openapi
+
+import com.virtuslab.dsl.{ Definition, Namespace }
+import com.virtuslab.interpreter.{ Context, Interpreter }
+import com.virtuslab.kubernetes.client.openapi.model
+
+object OpenAPI {
+
+  class OpenAPIContext extends Context {
+    override type Ret = Any // FIXME ?
+  }
+
+  implicit val context: OpenAPIContext = new OpenAPIContext
+
+  implicit val namespaceInterpreter: Interpreter[OpenAPIContext, Namespace[OpenAPIContext]] =
+    (namespace: Definition[OpenAPIContext, Namespace[OpenAPIContext]]) =>
+      Seq(
+        model.Namespace(
+          apiVersion = Some("v1"),
+          kind = Some("Namespace"),
+          metadata = Some(
+            model.ObjectMeta(
+              name = Some(namespace.obj.name),
+              labels = Some(namespace.obj.labels.toMap)
+            )
+          )
+        )
+      )
+
+}
---
+++ dsl/src/test/scala/com/virtuslab/interpreter/openapi/NamespaceInterpreterSpec.scala
@@ -0,0 +1,32 @@
+package com.virtuslab.interpreter.openapi
+
+import com.virtuslab.dsl.Definition
+import com.virtuslab.interpreter.InterpreterSpec
+import com.virtuslab.interpreter.openapi.OpenAPI.OpenAPIContext
+import com.virtuslab.kubernetes.client.openapi.model.Namespace
+import com.virtuslab.scalatest.json4s.jackson.JsonMatchers
+import com.virtuslab.scalatest.yaml.Converters.yamlToJson
+import org.json4s.jackson.Serialization
+import org.json4s.{ Formats, NoTypeHints }
+
+class NamespaceInterpreterSpec extends InterpreterSpec with JsonMatchers {
+
+  it should "serialize Namespace to JSON" in {
+    implicit val (ds, ns) = builders[OpenAPIContext]()
+
+    val namespace: Namespace = OpenAPI.namespaceInterpreter(Definition(ns.namespace)).head.asInstanceOf[Namespace] // FIXME
+
+    import org.json4s.jackson.Serialization._
+    implicit val formats: Formats = Serialization.formats(NoTypeHints)
+
+    write(namespace) should matchJson(yamlToJson(s"""
+        |---
+        |kind: Namespace
+        |apiVersion: v1
+        |metadata:
+        |  name: ${ns.name}
+        |  labels:
+        |    name: ${ns.name}
+        """.stripMargin))
+  }
+}
